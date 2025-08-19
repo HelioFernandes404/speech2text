f75d48f19a604a8687caa1466ab66dad173ef38e
heliofernandes404 <heliodevhub@gmail.com>
2025-08-19 14:23:15 -0300

    refactor: translating for en-us

--- src/processor.py
+++ src/processor.py
@@ -1,4 +1,4 @@
-# TODO: double check on en-us 
+# TODO: double check on en-us
 import os
 from .audio_converter import converter_mp3_to_wav, verify_audio
 from .transcriber import transcribe_audio
@@ -8,7 +8,7 @@ def processar_entrada(args):
     """Fluxo principal de processamento de áudio"""
     """Flow the main process the audio"""
     # Verificar arquivo de entrada
-    # Verifcy files the input 
+    # Verifcy files the input
     if not os.path.exists(args.input):
         raise FileNotFoundError(f"File {args.input} not found!")
 
@@ -16,25 +16,27 @@ def processar_entrada(args):
     wav_temp = None
 
     # Converter MP3 para WAV se necessário
-    # Converte the MP3 to WAV if necessrion 
-    if args.input.lower().endswith('.mp3'):
+    # Converte the MP3 to WAV if necessrion
+    if args.input.lower().endswith(".mp3"):
         wav_temp = os.path.splitext(args.input)[0] + ".wav"
         if not converter_mp3_to_wav(args.input, wav_temp):
-            raise RuntimeError("Fail on conversion MP3 to WAV") 
-        verify_audio(wav_temp) 
+            raise RuntimeError("Fail on conversion MP3 to WAV")
+        verify_audio(wav_temp)
         path_audio = wav_temp
-    # TODO: traduzir for en-us 
+    # TODO: traduzir for en-us
     # Transcrever áudio
     texto = transcribe_audio(path_audio, args.modelo, args.dispositivo)
 
     # Salvar transcrição
-    with open(args.saida, 'w', encoding='utf-8') as f:
+    # Save transcriber
+    with open(args.saida, "w", encoding="utf-8") as f:
         f.write(texto)
 
     # Limpar arquivo temporário
+    # Clear temp files
     if wav_temp and not args.manter_wav:
         try:
             os.remove(wav_temp)
-            print(f"🗑️ Arquivo temporário {wav_temp} removido")
+            print(f"🗑️ Temp files {wav_temp} removed")
         except Exception as e:
-            print(f"⚠️ Erro ao remover arquivo temporário: {str(e)}")
+            print(f"⚠️ Error at remove temp files: {str(e)}")
